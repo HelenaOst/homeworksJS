2fdcbb6dfc2ec3258b67aa677607946f978a4cc0
HelenaOst <greoizzi@gmail.com>
2025-04-26 14:43:06 +0300

    домашня робота №1

---
+++ Lesson1/main.js
@@ -0,0 +1,73 @@
+{//#dYQNrBV
+
+    let a = 'hello';
+    let b = 'owu';
+    let c = 'com';
+    let d = 'ua';
+
+    let e = 1;
+    let f = 10;
+    let g = -999;
+    let h = 123;
+    let i = 3.14;
+    let j = 2.7;
+    let k = 16;
+
+    let l = true;
+    let m = false;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+
+    console.log(e);
+    console.log(f);
+    console.log(g);
+    console.log(h);
+    console.log(i);
+    console.log(j);
+    console.log(k);
+
+    console.log(l);
+    console.log(m);
+}
+{
+//#6Qb97gsv
+
+    let firstName;
+    firstName = 'Олена';
+
+    let middleName;
+    middleName = 'Вікторівна';
+
+    let lastName;
+    lastName = 'Остапець';
+
+//варіант 1
+    let person = firstName + ' ' + middleName + ' ' + lastName;
+    console.log(person);
+
+// варіант 2
+    let person2 = `${firstName} ${middleName} ${lastName}`;
+    console.log(person2);
+}
+{
+//#4N0y5tufA
+    let a = 100;
+    let b = '100';
+    let c = true;
+
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+
+{
+// #ruUtWDUI
+
+    let firstName = prompt("Вкажіть своє імʼя:");
+    let middleName = prompt("Вкажіть своє імʼя по батькові:");
+    let yourAge = prompt("Вкажи свій вік:")
+    console.log(`Привіт, ${firstName} ${middleName}! Я знаю, що тобі ${yourAge} років.`);
+}
