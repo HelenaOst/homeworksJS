66b4b28aaf7b46f176c4313f5949b9263b412884
HelenaOst <greoizzi@gmail.com>
2025-05-03 20:01:36 +0300

    домашка по 3-й лекції

---
+++ Lesson3/task1/main.js
@@ -0,0 +1,13 @@
+// #y7crMeFwHcS
+//
+// – За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+for (let i = 1; i <= 10; i++) {
+    document.write(`<div><h2>Hello</h2></div>`);
+}
+
+//
+
+for (let i = 0; i < 10; i++) {
+    document.write(`<div><h2>Hello</h2></div>`);
+}
